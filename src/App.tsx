b9b7a86177fcba9be2ec207a9c9cf09a30bc0f76
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-01-19 17:48:55 +0500

    news letter section complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Newsletter from './components/NewsLetter'
 
 function App() {
 
   return (
     <body className=' mx-6 lg:mx-36'>
     <Navbar/>
+    <Newsletter/>
     <Footer/>
     </body>
   )
