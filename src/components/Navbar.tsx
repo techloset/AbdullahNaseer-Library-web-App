d3dba3b154f3c379c4a538ce3957a000da77efbe
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-01-19 16:06:45 +0500

    navbar complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import logo from "../assets/logo.svg";
 import hamBurger from "../assets/hamBurger.svg"
+import dropDown from "../assets/dropDown.svg"
+
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -10,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-col md:flex-row md:mx-16 justify-between items-center mt-6">
+    <nav className="flex flex-col md:flex-row justify-between items-center mt-6">
       <div className="flex justify-between md:flex w-full ">
         {/* Logo */}
         <img src={logo} alt="Logo" className="mr-4" />
@@ -27,11 +29,11 @@ const Navbar = () => {
           } md:flex flex-col md:flex-row md:space-x-12 mt-3 md:mt-0 transition-all ease-in-out duration-300`}
       >
         <a href="/">Home</a>
-        <a href="/menu">Landings</a>
-        <a href="/service">Pages</a>
+        <button className='flex ' >Landings <span className=''><img src={dropDown} alt="Logo" className="mr-4" /></span></button>
+        <button className='flex ' >Landings <span className=''><img src={dropDown} alt="Logo" className="mr-4" /></span></button>
         <a href="/cart">Docs</a>
         <a href="/cart">Help</a>
-        <a href="/cart">Search</a>
+        <button className='border-2 border-[#1565D8] text-[#1565D8] px-6 py-1 rounded-md'>Search</button>
       </div>
     </nav>
   );
