08a1e84a210c47e8a69cdda621fdafbe41264e7c
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-01-23 20:31:44 +0500

    refactoring for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App.tsx';
 import './index.css';
 import routes from './routes/routes.tsx'; // Import the routes file
