3666809c6da9c39d9277ba7b2f1e882b1a474c74
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-01-26 23:36:46 +0500

    finalizing for deployment

--- src/components/RelatedBook.tsx
+++ src/components/RelatedBook.tsx
@@ -38,86 +38,21 @@
 // export default RelatedBook;
 
 
-
-
-// import { useState, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import arrowLeft from '../assets/arrowLeft.svg';
-// import arrowRight from '../assets/arrowRight.svg';
-// import { AppDispatch, RootState } from '../redux/store';
-// import { fetchBooks } from '../redux/slices/books';
-// import { Link } from 'react-router-dom';
-
-// const RelatedBook: React.FC = () => {
-//   const dispatch: AppDispatch = useDispatch();
-//   const { isLoading, data, isError } = useSelector((state: RootState) => state.books);
-//   const [startIndex, setStartIndex] = useState(0);
-//   const [maxResults, setMaxResults] = useState(10);
-
-//   useEffect(() => {
-//     // Dispatch the fetchBooks action when the component mounts
-//     dispatch(fetchBooks({ startIndex, maxResults }));
-//   }, [dispatch, startIndex, maxResults]);
-//   const loadMoreResults = () => {
-//     setStartIndex(startIndex + maxResults);
-//     setMaxResults(maxResults + 10);
-//     dispatch(fetchBooks({ startIndex, maxResults: maxResults + 10 }));
-//   };
-//   const books = data?.items || [];
-//   return (
-//     <div className='parent bg-[#183B56] h-[548px] mt-7 md:mt-24 md:px-36'>
-//       <div className='flex justify-center md:justify-between pt-12 md:pt-14'>
-//         <div className='text text-white text-3xl font-bold italic'>Related Books</div>
-//         <div className='buttons hidden md:flex '>
-//           <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
-//             <img className='mx-auto' src={arrowLeft} alt='Left Arrow' />
-//           </button>
-//           <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
-//             <img className='mx-auto' src={arrowRight} alt='Right Arrow' />
-//           </button>
-//         </div>
-//       </div>
-//       <div className='slider mt-10 md:mt-14 overflow-x-auto whitespace-nowrap mx-auto no-scrollbar'>
-//         {books.map((book: any) => (
-//           <div key={book.id} className='inline-block mr-4 mb-5'>
-//             <Link to={`/bookdetails/${book.id}`}>
-//               <div className='inline-block h-[298px] w-[204px] bg-red-500 rounded-md mr-4'>
-//                 <img className='object-cover' src={book.volumeInfo.imageLinks.thumbnail} alt="picture" />
-//               </div>
-//             </Link>
-//           </div>
-//         ))}
-//       </div>
-//       <div className='buttons flex md:hidden justify-center mt-3 '>
-//         <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
-//           <img className='mx-auto' src={arrowLeft} alt='Left Arrow' />
-//         </button>
-//         <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
-//           <img className='mx-auto' src={arrowRight} alt='Right Arrow' />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RelatedBook;
-
-
-
-
-import { useState, useEffect } from 'react';
+import {useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux/store';
 import { fetchBooks } from '../redux/slices/books';
 import { Link } from 'react-router-dom';
 import arrowLeft from '../assets/arrowLeft.svg';
 import arrowRight from '../assets/arrowRight.svg';
+import Loader from './Loader';
 
 const RelatedBook: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const { isLoading, data, isError } = useSelector((state: RootState) => state.books);
-  const [startIndex, setStartIndex] = useState(0);
-  const [maxResults, setMaxResults] = useState(10);
+
+  const startIndex = 0
+  const maxResults = 10
 
   useEffect(() => {
     dispatch(fetchBooks({ startIndex, maxResults }));
@@ -139,8 +74,8 @@ const RelatedBook: React.FC = () => {
         </div>
       </div>
 
-      {isLoading && <div className='text-white'>Loading...</div>}
-      {isError && <div className='text-white'>Error loading books.</div>}
+      {isLoading && <div className='text-white'><Loader/></div>}
+      {isError && <div className='text-white'>Error loading books</div>}
 
       {!isLoading && !isError && (
         <div className='slider mt-10 md:mt-14 overflow-x-auto whitespace-nowrap mx-auto no-scrollbar'>
@@ -148,7 +83,7 @@ const RelatedBook: React.FC = () => {
             <div key={book.id} className='inline-block mr-4 mb-5'>
               <Link to={`/bookdetails/${book.id}`}>
                 <div className='inline-block h-[298px] w-[204px] bg-red-500 rounded-md mr-4'>
-                  <img className='object-cover' src={book.volumeInfo.imageLinks?.thumbnail} alt='Book Cover' />
+                  <img className='object-cover inline-block h-[298px] w-[204px] bg-red-500 rounded-md' src={book.volumeInfo.imageLinks?.thumbnail} alt='Book Cover' />
                 </div>
               </Link>
             </div>
