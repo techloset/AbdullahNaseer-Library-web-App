f0219424bb877dce616cdec587ed370af26894d4
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-01-29 16:53:21 +0500

    eft and right button functionality implementation

--- src/components/RelatedBook.tsx
+++ src/components/RelatedBook.tsx
@@ -1,4 +1,4 @@
-import {useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux/store';
 import { fetchBooks } from '../redux/slices/books';
@@ -11,24 +11,37 @@ const RelatedBook: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const { isLoading, data, isError } = useSelector((state: RootState) => state.books);
 
-  const startIndex = 0
-  const maxResults = 10
+  const startIndex = 0;
+  const maxResults = 10;
 
   useEffect(() => {
     dispatch(fetchBooks({ startIndex, maxResults }));
   }, [dispatch, startIndex, maxResults]);
 
   const books = data?.items || [];
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft -= 200; // Adjust the scroll amount as needed
+    }
+  };
+
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft += 200; // Adjust the scroll amount as needed
+    }
+  };
 
   return (
     <div className='parent bg-[#183B56] h-full mt-7 md:mt-24 md:px-36'>
       <div className='flex justify-center md:justify-between pt-12 md:pt-14'>
         <div className='text text-white text-3xl font-bold italic'>Related Books</div>
         <div className='buttons hidden md:flex '>
-          <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
+          <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full' onClick={scrollLeft}>
             <img className='mx-auto' src={arrowLeft} alt='Left Arrow' />
           </button>
-          <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
+          <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full' onClick={scrollRight}>
             <img className='mx-auto' src={arrowRight} alt='Right Arrow' />
           </button>
         </div>
@@ -38,7 +51,7 @@ const RelatedBook: React.FC = () => {
       {isError && <div className='text-white'>Error loading books</div>}
 
       {!isLoading && !isError && (
-        <div className='slider mt-10 md:mt-14 overflow-x-auto whitespace-nowrap mx-auto no-scrollbar'>
+        <div className='slider mt-10 md:mt-14 overflow-x-auto whitespace-nowrap mx-auto no-scrollbar' ref={sliderRef}>
           {books.map((book: any) => (
             <div key={book.id} className='inline-block mr-4 mb-5'>
               <Link to={`/bookdetails/${book.id}`}>
@@ -52,10 +65,10 @@ const RelatedBook: React.FC = () => {
       )}
 
       <div className='buttons flex md:hidden justify-center mt-3'>
-        <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
+        <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full' onClick={scrollLeft}>
           <img className='mx-auto' src={arrowLeft} alt='Left Arrow' />
         </button>
-        <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full'>
+        <button className='h-14 w-14 border-2 mx-3 p-2 rounded-full' onClick={scrollRight}>
           <img className='mx-auto' src={arrowRight} alt='Right Arrow' />
         </button>
       </div>
