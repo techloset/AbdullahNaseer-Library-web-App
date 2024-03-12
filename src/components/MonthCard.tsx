b4e7dfa488f24c5175f6c7a577d35ce968baf3a0
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-03-12 07:11:07 +0500

    month card finalize for QA

--- src/components/MonthCard.tsx
+++ src/components/MonthCard.tsx
@@ -1,62 +1,60 @@
-
-import React from 'react';
-import {Book} from "../types/types"
+import React from "react";
+import { Book } from "../types/types";
 
 interface CardProps {
-    book: Book;
+  book: Book;
 }
 
 const defaultBook: Book = {
-    id: '',
-    volumeInfo: {
-        title: 'Title Not Available',
-        authors: ['Authors Not Available'],
-        publishedDate: 'Published Date Not Available',
-        pageCount: 0,
-        printType: 'Print Type Not Available',
-        maturityRating: 'Maturity Rating Not Available',
-        imageLinks: {
-            smallThumbnail: 'fallback-image-url',
-            thumbnail: 'fallback-image-url',
-        },
+  id: "",
+  volumeInfo: {
+    title: "Title Not Available",
+    authors: ["Authors Not Available"],
+    publishedDate: "Published Date Not Available",
+    pageCount: 0,
+    printType: "Print Type Not Available",
+    maturityRating: "Maturity Rating Not Available",
+    imageLinks: {
+      smallThumbnail: "fallback-image-url",
+      thumbnail: "fallback-image-url",
     },
+  },
 };
 const trimText = (text: string, wordCount: number): string => {
-    const words = text.split(' ');
-    if (words.length > wordCount) {
-        return words.slice(0, wordCount).join(' ') + '...';
-    }
-    return text;
+  const words = text.split(" ");
+  if (words.length > wordCount) {
+    return words.slice(0, wordCount).join(" ") + "...";
+  }
+  return text;
 };
 
-
 const MonthCard: React.FC<CardProps> = ({ book }) => {
-    const mergedBook = { ...defaultBook, ...book };
-    const {
-        title,
-        authors,
-        maturityRating,
-        imageLinks,
-    } = mergedBook.volumeInfo!;
-    const trimmedTitle = trimText(title || '', 5);
+  const mergedBook = { ...defaultBook, ...book };
+  const { title, maturityRating, imageLinks } = mergedBook.volumeInfo!;
+  const trimmedTitle = trimText(title || "", 3);
 
-    return (
-        <div className="relative w-[270px] h-[312px] md:w-[300px] md:h-[260px] rounded-md md:flex md:flex-row border-2 md:border-0">
-            <div className="absolute md:static w-[92px] h-[137px] left-20 top-[-30px] md:h-auto md:w-auto md:flex-1">
-                <img className=' bg-red-500 md:ml-2 w-full h-full md:h-2/3 object-cover md:mt-10 rounded-md' src={imageLinks?.thumbnail} alt="picture" />
-            </div>
-            <div className="mt-[132px] md:mt-0 md:flex-1 p-4 flex flex-col justify-between text-center md:text-left">
-                <div>
-                    <p className="text-lg font-bold italic text-customTextColour md:mb-2">{trimmedTitle}</p>
-                    <p className="text-customTextColour text-base font-thin">{authors && authors.length > 0 ? authors[0] : 'Author Not Available'}</p>
-
-                    <p className="text-linkColour italic text-sm md:mt-6 font-bold">{`$${maturityRating ? 'Not For Sale' : 'Price Not Available'}`}</p>
-                </div>
-                    <button className=" bg-customTextColour text-white text-sm px-4 py-2 w-[101px] h-9 rounded-full">Buy Now</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-between">
+      <div className="w-[95px] h-[141px] md:w-[119px] md:h-[177px] bg-slate-200 border-1 rounded-sm relative">
+        <img
+          className="absolute inset-0 w-full h-full object-cover"
+          src={imageLinks?.thumbnail}
+          alt="picture"
+        />
+      </div>
+      <div className="text-start w-[103px] h-[48px] md:w-[165px] md:h-[64px] ml-6">
+        <p className="text-lg md:text-2xl font-bold italic text-customTextColour md:mb-2">
+          {trimmedTitle}
+        </p>
+        <p className="text-linkColour italic text-sm md:text-lg mt-3 md:mt-6 font-bold">{`$${
+          maturityRating ? "Not For Sale" : "Price Not Available"
+        }`}</p>
+        <button className="bg-customTextColour text-white text-sm px-4 py-2 mt-5 md:mt-10 w-[101px] h-9 rounded-full">
+          Buy Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MonthCard;
-
